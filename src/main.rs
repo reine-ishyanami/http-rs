fe5ff98dce2775b50e201f90400481d6061cffa9
reine-ishyanami <2402979195@qq.com>
2024-02-13 17:59:33 +0800

    读取启动参数

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,55 @@
 use std::{
+    env,
     error::Error,
     fs::File,
-    io::{Read, Write},
+    io::{Read, Write}, path::Path,
 };
 
-mod server;
 mod entity;
+mod server;
 
 use crate::entity::Server;
 
 use crate::server::handle;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let file_name = "api.yml";
-    match File::open(file_name) {
+async fn main() {
+    handle_program_args().await;
+}
+
+///
+/// 处理程序启动参数
+///
+async fn handle_program_args() {
+    let args: Vec<String> = env::args().collect();
+
+    let a1 = args.get(1);
+    let a2 = args.get(2);
+    match a1 {
+        Some(cmd) => match cmd.as_str() {
+            "--help" | "-h" => println!("{}", get_help()),
+            "--file" | "-f" => match a2 {
+                Some(file_path) => {
+                    if let Err(e) = start(file_path, false).await {
+                        panic!("程序启动异常{}", e);
+                    }
+                }
+                None => eprintln!("请指定配置文件路径"),
+            },
+            _ => eprintln!("启动参数错误"),
+        },
+        None => {
+            if let Err(e) = start("api.yml", true).await {
+                panic!("程序启动异常{}", e);
+            }
+        }
+    }
+}
+
+///
+/// 启动服务器
+async fn start(file_name: &str, default: bool) -> Result<(), Box<dyn Error>> {
+    match File::open(Path::new(file_name)) {
         Ok(f) => {
             let mut file = f;
             // 读取文件内容到字符串
@@ -25,12 +60,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
             handle(server).await;
         }
         Err(_) => {
-            eprintln!("系统找不到指定的文件，已根据默认值生成");
-            let server: Server = Server::default();
-            let server_str = serde_yaml::to_string(&server).unwrap();
-            let mut file = File::create(file_name).unwrap();
-            file.write_all(server_str.as_bytes()).unwrap();
+            if default {
+                eprintln!("系统找不到指定的文件，已根据默认值生成");
+                let server: Server = Server::default();
+                let server_str = serde_yaml::to_string(&server).unwrap();
+                let mut file = File::create(file_name).unwrap();
+                file.write_all(server_str.as_bytes()).unwrap();
+            } else {
+                eprintln!("系统找不到指定的文件");
+            }
         }
     }
     Ok(())
+}
+
+///
+///
+/// 帮助信息
+fn get_help() -> String {
+    let help_msg = r#"
+http-server [option]
+
+option:
+    -h, --help: 查看帮助信息
+    -f, --file <path>: 指定配置文件路径
+    "#;
+    String::from(help_msg)
 }
